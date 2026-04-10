d7b0f9aecc966b04a6030a61713e3df7b313be66
joel <joel@example.com>
2026-04-10 22:43:59 +0800

    fix: simplify MainApplication.kt, rely on autolink

--- android/app/src/main/java/com/lingshu/paipan/MainApplication.kt
+++ android/app/src/main/java/com/lingshu/paipan/MainApplication.kt
@@ -1,19 +1,14 @@
 package com.lingshu.paipan
 
 import android.app.Application
-import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
-import com.facebook.react.ReactPackage
 import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
       object : ReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> =
-            PackageList(this).packages
-
         override fun getJSMainModuleName(): String = "index"
 
         override fun getUseDeveloperSupport(): Boolean = false
