c2909ff0bc61488e24f95d5381d323aa9e473d10
joel <joel@example.com>
2026-04-11 07:28:09 +0800

    feat: upgrade react-native-screens to 4.24.0 and use standard RN autolink configuration

--- android/app/src/main/java/com/lingshu/paipan/MainApplication.kt
+++ android/app/src/main/java/com/lingshu/paipan/MainApplication.kt
@@ -3,6 +3,7 @@ package com.lingshu.paipan
 import android.app.Application
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
 import com.facebook.react.shell.MainReactPackage
 import com.facebook.soloader.SoLoader
 
@@ -10,8 +11,8 @@ class MainApplication : Application(), ReactApplication {
 
   override fun getReactNativeHost(): ReactNativeHost =
       object : ReactNativeHost(this) {
-        override fun getPackages(): List<com.facebook.react.ReactPackage> =
-            listOf<com.facebook.react.ReactPackage>(MainReactPackage())
+        override fun getPackages(): List<ReactPackage> =
+            listOf<ReactPackage>(MainReactPackage())
 
         override fun getJSMainModuleName(): String = "index"
 
