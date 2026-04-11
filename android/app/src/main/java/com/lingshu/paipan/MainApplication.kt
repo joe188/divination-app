2617fda799591704a42d5c7dad483fdca776178e
joel <joel@example.com>
2026-04-11 14:19:19 +0800

    upgrade: RN 0.73.8 + react-native-screens 4.2.0 + New Architecture (Fabric). Clean autolink config.

--- android/app/src/main/java/com/lingshu/paipan/MainApplication.kt
+++ android/app/src/main/java/com/lingshu/paipan/MainApplication.kt
@@ -1,12 +1,10 @@
 package com.lingshu.paipan
 
 import android.app.Application
+import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.shell.MainReactPackage
-import com.swmansion.rnscreens.ScreensPackage
-import com.th3rdwave.safeareacontext.SafeAreaContextPackage
 import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
@@ -14,15 +12,11 @@ class MainApplication : Application(), ReactApplication {
   override fun getReactNativeHost(): ReactNativeHost =
       object : ReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> =
-            listOf<ReactPackage>(
-                MainReactPackage(),
-                ScreensPackage(),
-                SafeAreaContextPackage()
-            )
+            PackageList(this).packages
 
         override fun getJSMainModuleName(): String = "index"
 
-        override fun getUseDeveloperSupport(): Boolean = false
+        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
       }
 
   override fun onCreate() {
